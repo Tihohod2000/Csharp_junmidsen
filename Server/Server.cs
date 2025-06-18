eb38cfb114ec42da169678b6ca95ccbba4cb9140
Tihohod2000 <dumanovskiy15@gmail.com>
2025-06-18 20:59:17 +0300

    Created class Server

--- Server/Server.cs
+++ Server/Server.cs
@@ -2,5 +2,19 @@ namespace Server;
 
 public class Server
 {
+    private static int count = 0;
+    private static readonly ReaderWriterLockSlim rwLockSlim = new ReaderWriterLockSlim();
+
+    public static int GetCount()
+    {
+        return count;
+    }
+
+
+    public static void AddCount(int value)
+    {
+        count += value;
+    }
+    
     
 }
