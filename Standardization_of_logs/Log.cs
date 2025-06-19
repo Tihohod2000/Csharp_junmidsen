0daf3d5bf0d895b937541e7f7ce118ab918f9cde
Tihohod2000 <dumanovskiy15@gmail.com>
2025-06-19 12:31:56 +0300

    added class log

---
+++ Standardization_of_logs/Log.cs
@@ -0,0 +1,10 @@
+namespace Standardization_of_logs;
+
+public class Log
+{
+    public DateTime data { get; set; }
+    public string lvlLog { get; set; }
+    public string method = "DEFAULT";
+    public string message { get; set; }
+    
+}
--- Standardization_of_logs/Program.cs
+++ Standardization_of_logs/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        
+        string[] logs = File.ReadAllLines("../../../Logs.txt");
+        // Console.WriteLine(lines.GetValue(1));
+        string correctLogs = "correctLogs.txt";
+        string problems = "problems.txt";
     }
 }
