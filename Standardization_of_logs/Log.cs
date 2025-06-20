7dd7649ce3761aa1e6db19c865b1c6fd46d2d789
Tihohod2000 <dumanovskiy15@gmail.com>
2025-06-20 13:58:05 +0300

    Fix regex & refactoring

--- Standardization_of_logs/Log.cs
+++ Standardization_of_logs/Log.cs
@@ -11,54 +11,66 @@ public class Log
     public string message { get; set; }
 
 
+    public Match[] tryParseInfo(string log)
+    {
+        var dataTimeRegex = new Regex(
+            @"(?<datetime>(?<date>\d{4}-\d{2}-\d{2}|\d{2}\.\d{2}\.\d{4})\s+(?<time>\d{2}:\d{2}:\d{2}(?:\.\d{1,4})?))\s*", RegexOptions.ExplicitCapture);
+
+        var levelRegex = new Regex(@"(?<level>INFO\w*|WARN\w*|ERROR|DEBUG)\b\s*");
+
+        var methodRegex = new Regex(@"(?<method>[^|][A-Za-z]+[.]+[A-Za-z]+)\s*", RegexOptions.ExplicitCapture);
+
+        var messageRegex = new Regex(@"(?<message>[А-Яа-я]+\s+[А-Яа-я]+[:]+.*)$");
+
+        Match[] arrayMatch = 
+        [
+            dataTimeRegex.Match(log),
+            levelRegex.Match(log),
+            methodRegex.Match(log),
+            messageRegex.Match(log)
+        ];
+
+        return arrayMatch;
+    }
+    
     public bool IsValid(string? log)
     {
         if (log == null) return false;
+        
+        Match[] arrayMatch = tryParseInfo(log);
 
-        var regex = new Regex(
-            @"(?<datetime>(?<date>\d{4}-\d{2}-\d{2}|\d{2}\.\d{2}\.\d{4})\s+" +
-            @"(?<time>\d{2}:\d{2}:\d{2}(?:\.\d{1,4})?))\s*" +
-            @"(?:\s*\|\s*[^|]*)*?" +
-            @"(?<level>INFORMATION|INFO|WARNING|WARN|ERROR|DEBUG)\b\s*" +
-            @"(?:\s*\|\s*(?<method>[A-Za-z0-9_.]+)\s*)*" +
-            @"(?:\s*\|\s*|\s*)(?<message>.+)$",
-            RegexOptions.ExplicitCapture);
+        int countCorrect = 0;
+        foreach (var Match in arrayMatch)
+        {
+            if (Match.Success) countCorrect++;
+        }
 
-        var match = regex.Match(log);
-
-        Console.WriteLine(match.Groups["method"].Value);
-        // Console.WriteLine(String.Join(" ",match.Groups.Keys));
-
-        return regex.IsMatch(log);
+        if (countCorrect >= 3) return true;
+        return false;
     }
 
     public void ParseFromLog(string? log)
     {
-        // if (log == null) return ;
-        // // if (log == null) return false;
-        // if (!IsValid(log))
-        // {
-        //     return;
-        // }
+        if (log == null) return ;
+        
 
-        var regex = new Regex(
-            @"(?<datetime>(?<date>\d{4}-\d{2}-\d{2}|\d{2}\.\d{2}\.\d{4})\s+" +
-            @"(?<time>\d{2}:\d{2}:\d{2}(?:\.\d{1,4})?))\s*" +
-            @"(?:\s*\|\s*[^|]*)*?" +
-            @"(?<level>INFORMATION|INFO|WARNING|WARN|ERROR|DEBUG)\b\s*" +
-            @"(?:\s*\|\s*(?<method>[A-Za-z0-9_.]+)\s*)*" +
-            @"\s*[|]*(?<message>\s*.[^|]+)$",
-            RegexOptions.ExplicitCapture);
+        var dataTimeRegex = new Regex(
+            @"(?<datetime>(?<date>\d{4}-\d{2}-\d{2}|\d{2}\.\d{2}\.\d{4})\s+(?<time>\d{2}:\d{2}:\d{2}(?:\.\d{1,4})?))\s*", RegexOptions.ExplicitCapture);
 
-        var match = regex.Match(log);
-        // IEnumerable<string> groupsMatch = match.Groups.Keys;
+        var levelRegex = new Regex(@"(?<level>INFO\w*|WARN\w*|ERROR|DEBUG)\b\s*");
 
+        var methodRegex = new Regex(@"(?<method>[^|][A-Za-z]+[.]+[A-Za-z]+)\s*", RegexOptions.ExplicitCapture);
+
+        var messageRegex = new Regex(@"(?<message>[А-Яа-я]+\s+[А-Яа-я]+[:]+.*)$");
+
+        Match[] arrayMatch = tryParseInfo(log);
+        
         string[] formats = {
             "yyyy-MM-dd HH:mm:ss.ffff",
             "dd.MM.yyyy HH:mm:ss.fff"
         };
 
-        if (DateTime.TryParseExact(match.Groups["datetime"].Value,
+        if (DateTime.TryParseExact(arrayMatch[0].Groups["datetime"].Value,
                 formats, CultureInfo.InvariantCulture,
                 DateTimeStyles.None,
                 out DateTime result
@@ -67,17 +79,13 @@ public class Log
             data = result;
         }
         
-        // data = DateTime.ParseExact(
-        //     match.Groups["datetime"].Value,
-        //     "dd.MM.yyyy HH:mm:ss.fff",
-        //     CultureInfo.InvariantCulture);
-        lvlLog = match.Groups["level"].Value;
-        if (match.Groups["method"].Success)
+        lvlLog = arrayMatch[1].Groups["level"].Value;
+        if (arrayMatch[2].Groups["method"].Success)
         {
-            method = match.Groups["method"].Value;
+            method = arrayMatch[2].Groups["method"].Value;
         }
 
-        message = match.Groups["message"].Value;
+        message = arrayMatch[3].Groups["message"].Value;
     }
 
 
